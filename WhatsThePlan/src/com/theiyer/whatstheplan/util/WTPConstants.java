4bd622761ad1b38d85fa5b23de67ebae037db971
Gaurav <gaurav.saheja@gmail.com>
2014-04-11 23:50:26 +0530

    commiting constant

--- WhatsThePlan/src/com/theiyer/whatstheplan/util/WTPConstants.java
+++ WhatsThePlan/src/com/theiyer/whatstheplan/util/WTPConstants.java
@@ -2,7 +2,7 @@ package com.theiyer.whatstheplan.util;
 
 public interface WTPConstants {
 	
-	//public static final String TARGET_HOST = "103.29.97.131";
+	//public static final String TARGET_HOST = "103.250.16.18";
 	public static final String TARGET_HOST =  "ec2-54-200-227-62.us-west-2.compute.amazonaws.com";
 	//public static final String TARGET_HOST = "www.theiyersolutions.com";
 	
