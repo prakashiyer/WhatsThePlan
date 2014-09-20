55a0d32012d3585acf334d4009b3b42a57a522f3
Prakash Iyer <prakashk16@gmail.com>
2014-09-20 15:34:40 +0530

    CenterList

---
+++ WhatsThePlan/src/com/theiyer/whatstheplan/entity/CenterList.java
@@ -0,0 +1,20 @@
+package com.theiyer.whatstheplan.entity;
+
+import java.util.List;
+
+public class CenterList
+{
+	private List<Center> centers;
+	
+	public CenterList() {
+	}
+	
+	public List<Center> getCenters() {
+		return centers;
+	}
+	
+	public void setCenters(List<Center> centers) {
+		this.centers = centers;
+	}
+}
+
